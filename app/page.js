bb216e744c5dd3c6a5877270ef683cae85e5ea91
codeoz <ozudev@gmail.com>
2024-03-06 16:24:34 +0300

    test

--- app/page.js
+++ app/page.js
@@ -3,12 +3,11 @@
 import Left from '@/components/Left'
 import Hero from '@/components/Hero'
 
-
 export default function Home() {
   return (
     <main className='grid grid-cols-4 container mx-auto' id='home'>
       <Left />
-      <Hero />
+      <Hero />a
     </main>
   )
 }
