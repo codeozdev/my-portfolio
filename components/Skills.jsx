4a113f1060b405ffc624a9ead3ded78ffd7b843d
codeoz <ozudev@gmail.com>
2024-03-28 23:15:46 +0300

    Added new styles and improved existing styles

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -3,122 +3,272 @@ import Image from 'next/image'
 export default function Skills() {
   return (
     <div className='start text-center xl:text-start' id='skills'>
-      <p className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand uppercase'>What I do? </p>
+      <p className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand uppercase'>
+        What I do?{' '}
+      </p>
       <h1>Here are some of my expertise</h1>
-      <div className=''>
+      <div>
         {/* FRONTEND */}
         <h1 className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand uppercase mt-10 mb-3'>
           FRONTEND
         </h1>
-        <div className='grid grid-cols-3 gap-1 sm:gap-3 place-items-center xl:flex xl:gap-3 xl:items-center'>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/html.png'} alt='html' width={200} height={200} quality={100} />
-            <p className='styleName'>HTML</p>
+        <div className='grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-1 w-full h-full'>
+          <div className='icons-div'>
+            <Image
+              src={'/frontend/html.png'}
+              alt='html'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>HTML</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/javascript.png'} alt='javascript' width={200} height={200} quality={100} />
-            <p className='styleName'>JavaScript</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/frontend/javascript.png'}
+              alt='javascript'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>JavaScript</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/typescript.png'} alt='typescript' width={200} height={200} quality={100} />
-            <p className='styleName'>TypeScript</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/frontend/typescript.png'}
+              alt='typescript'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>TypeScript</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/react.png'} alt='react' width={200} height={200} quality={100} />
-            <p className='styleName'>React</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/frontend/react.png'}
+              alt='react'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>React</p>
           </div>
-          <div className='shadow-2xl shadow-blue-500 text-center myStyle border-b-2 border-blue-500'>
-            <Image src={'/next.png'} alt='next' width={200} height={200} quality={100} />
-            <p className='styleName'>Nextjs</p>
+
+          <div className='icons-div lg:!shadow-red-500'>
+            <Image
+              src={'/frontend/next.png'}
+              alt='next'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Nextjs</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/redux.png'} alt='redux' width={200} height={200} quality={100} />
-            <p className='styleName'>Redux</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/frontend/redux.png'}
+              alt='redux'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Redux</p>
           </div>
         </div>
+
         {/* STYLE */}
         <h1 className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand uppercase mt-10 mb-3'>
           styling
         </h1>
-        <div className='grid grid-cols-3 gap-1 sm:gap-3 place-items-center xl:flex xl:gap-3 xl:items-center'>
-          <div className='shadow-2xl shadow-blue-500 text-center border-b-2 border-blue-500 myStyle'>
-            <Image src={'/tailwind.png'} alt='tailwind' width={200} height={200} quality={100} />
-            <p className='styleName'>Tailwind</p>
+        <div className='grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-1 w-full h-full'>
+          <div className='icons-div lg:!shadow-red-500'>
+            <Image
+              src={'/style/tailwind.png'}
+              alt='tailwind'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Tailwind</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/bootstrap.png'} alt='bootstrap' width={200} height={200} quality={100} />
-            <p className='styleName'>Bootstrap</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/style/bootstrap.png'}
+              alt='bootstrap'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Bootstrap</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/mui.png'} alt='material' width={200} height={200} quality={100} />
-            <p className='styleName'>Material</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/style/mui.png'}
+              alt='material'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Material</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/semantic.png'} alt='semantic' width={200} height={200} quality={100} />
-            <p className='styleName'>Semantic</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/style/semantic.png'}
+              alt='semantic'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Semantic</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/daisy.png'} alt='daisy' width={200} height={200} quality={100} />
-            <p className='styleName'>Daisy UI</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/style/daisy.png'}
+              alt='daisy'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Daisy</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/sass.png'} alt='sass' width={200} height={200} quality={100} />
-            <p className='styleName'>Sass</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/style/sass.png'}
+              alt='sass'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Sass</p>
           </div>
         </div>
+
         {/* BACKEND */}
         <h1 className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand uppercase mt-10 mb-3'>
           BACKEND
         </h1>
-        <div className='grid grid-cols-3 gap-1 sm:gap-3 place-items-center xl:flex xl:gap-3 xl:items-center'>
-          <div className='shadow-2xl shadow-blue-500 text-center border-b-2 border-blue-500 myStyle'>
-            <Image src={'/nodejs.png'} alt='nodejs' width={200} height={200} quality={100} className='p-3 sm:px-0' />
-            <p className='styleName'>Nodejs</p>
-          </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/mongodb.png'} alt='mongodb' width={200} height={200} quality={100} className='p-3 sm:px-0' />
-            <p className='styleName'>MongoDB</p>
-          </div>
-          <div className='shadow-2xl text-center myStyle gap-1 sm:gap-3'>
+        <div className='grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-1 w-full h-full'>
+          <div className='icons-div'>
             <Image
-              src={'/nextauth.png'}
+              src={'/backend/nodejs.png'}
+              alt='nodejs'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Nodejs</p>
+          </div>
+
+          <div className='icons-div'>
+            <Image
+              src={'/backend/mongodb.png'}
+              alt='mongodb'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>MongoDB</p>
+          </div>
+
+          <div className='icons-div'>
+            <Image
+              src={'/backend/nextauth.png'}
               alt='nextauth'
               width={200}
               height={200}
               quality={100}
-              className='p-3 sm:px-0'
+              className='icons-img'
             />
-            <p className='styleName'>NextAuth</p>
+            <p className='icons-p'>NextAuth</p>
           </div>
-          <div className='shadow-2xl text-center myStyle'>
-            <Image src={'/jwt.png'} alt='jwt' width={200} height={200} quality={100} className='p-3 sm:px-0' />
-            <p className='styleName'>JWT</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/backend/jwt.png'}
+              alt='jwt'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>JWT</p>
           </div>
         </div>
+
         {/* EXTRA */}
         <h1 className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand uppercase mt-10 mb-3'>
           EXTRA
         </h1>
-        <div className='grid grid-cols-3 gap-1 sm:gap-3 place-items-center xl:flex xl:gap-3 xl:items-center'>
-          <div className='shadow-2xl text-center p-5 myStyle'>
-            <Image src={'/c.png'} alt='c#' width={200} height={200} quality={100} />
-            <p className='styleName'>C#</p>
+        <div className='grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-1 w-full h-full'>
+          <div className='icons-div'>
+            <Image
+              src={'/extra/c.png'}
+              alt='c#'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>C#</p>
           </div>
-          <div className='shadow-2xl text-center p-5 myStyle'>
-            <Image src={'/python.png'} alt='python' width={200} height={200} quality={100} />
-            <p className='styleName'>Python</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/extra/python.png'}
+              alt='python'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Python</p>
           </div>
-          <div className='shadow-2xl text-center p-5 myStyle'>
-            <Image src={'/git.png'} alt='git' width={200} height={200} quality={100} />
-            <p className='styleName'>Git</p>
+
+          <div className='icons-div lg:!shadow-red-500'>
+            <Image
+              src={'/extra/linux.png'}
+              alt='linux'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Linux</p>
           </div>
-          <div className='shadow-2xl text-center p-5 myStyle'>
-            <Image src={'/figma.png'} alt='figma' width={200} height={200} quality={100} />
-            <p className='styleName'>Figma</p>
-          </div>
-          <div className='shadow-2xl shadow-blue-500 text-center p-5 myStyle border-b-2 border-blue-500'>
-            <Image src={'/linux.png'} alt='linux' width={200} height={200} quality={100} />
-            <p className='styleName'>Linux</p>
+
+          <div className='icons-div'>
+            <Image
+              src={'/extra/git.png'}
+              alt='git'
+              width={200}
+              height={200}
+              quality={100}
+              className='icons-img'
+            />
+            <p className='icons-p'>Git</p>
           </div>
         </div>
       </div>
