70241a92de7d5d2c6bc911a1d39bc66a5d9ddee1
codeoz <ozudev@gmail.com>
2024-03-30 01:48:16 +0300

    Changed font size

--- components/About.jsx
+++ components/About.jsx
@@ -1,12 +1,17 @@
 export default function About() {
   return (
     <div className='start text-center lg:text-start' id='about'>
-      <p className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand'>ABOUT US </p>
+      <p className='text-[10px] font-thin text-neutral-600 tracking-[0.35em] font-sand'>
+        ABOUT US{' '}
+      </p>
       <h1>WHO AM I?</h1>
-      <p className='tracking-[0.10em] leading-7 text-sm font-sand text-neutral-600 select-none'>
-        <strong className='font-extrabold tracking-normal'>Hi I{`'`}m Oguzhan Uguz </strong>
-        <br />I have been working in both frontend and backend development for 1.5 years, and I am passionate about
-        keeping up with industry inn ovations and adapting to new technologies. I am confident in my ability to solve
+      <p className='tracking-[0.10em] leading-7 text-sm md:text-base font-sand text-neutral-600 select-none'>
+        <strong className='font-extrabold text-base tracking-normal block md:mb-2'>
+          Hi I{`'`}m Oguzhan Uguz{' '}
+        </strong>
+        I have been working in both frontend and backend development for 1.5
+        years, and I am passionate about keeping up with industry inn ovations
+        and adapting to new technologies. I am confident in my ability to solve
         problems and produce efficient solutions.
       </p>
     </div>
